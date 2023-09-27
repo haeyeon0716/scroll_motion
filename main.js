2be751d87394510b383fa3872196f31885042f87
yhy7805 <yhy7805@gmail.com>
2023-09-27 13:52:07 +0900

    테두리 모션 위한 스타일링

--- main.js
+++
@@ -1,9 +0,0 @@
-const box = document.querySelector('article');
-
-box.addEventListener('click', ()=>{
-    new Anime(box,
-         {left: 1000, backgroundColor:'#fcfda4'},
-         {duration: 1000, easeType: 'linear', callback:()=>{
-            new Anime(box, {top:300, background: '#70ffcf'})
-         }});
-})
