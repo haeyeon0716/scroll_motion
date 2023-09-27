2e814c66f14bdc71cd233a08febef1e8784c4605
yhy7805 <yhy7805@gmail.com>
2023-09-27 10:38:01 +0900

    특정 영역에 도달했을때 스크롤 적용되는 값만 3항연산자로 간소하게 처리

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
 /*
-window.scrollY : 스크롤시 스크롤 되는 거리값 (동적)
-DOM.offset().top : 전체 페이지에서 특정 DOM의 세로 위치값 (정적)
+  window.scrollY : 브라우저 스크롤시 스크롤되는 거리값 (동적)
+  DOM.offsetTop : 전체 페이지에 특정 DOM의 세로 위치값 (정적)
 */
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h1_2 = secs[1].querySelector('h1');
-//baseline = box의 모션 시작 시점을 기본 box영역에서 300만큼 올림
-const baseline = -300;
+//baseLine은 박스의 모션 시작시점을 기존 박스 영역에서 300만큼 위로 올림
+const baseLine = -300;
 
-window.addEventListener('scroll', ()=>{
-    const scroll = window.scrollY;
-    //박스의 실 적용되는 스크롤 수치값은 반대로 300만큼 더해줘야 함
-    const scroll_2 = scroll - secs[1].offsetTop - baseline;
-    const initscroll = 0;
+window.addEventListener('scroll', () => {
+	const scroll = window.scrollY;
+	//박스에 실 적용되는 스크롤 수치값을 반대로 300만큼 더해줘야됨
 
-    h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1+ scroll/200})`
-    h1.style.opacity = 1 - scroll/400;
+	const initScroll = 0;
 
-    if(scroll > secs[1].offsetTop + baseline){
-        h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${1+ scroll_2/200})`
-        h1_2.style.opacity = 1 - scroll_2/400;
-    }else{
-        //원래 위치로 스크롤시 조금싹 오차 범위가 발생하기 때문에 원래값으로 강제 보정처리
-        h1_2.style.transform = `translateX(${initscroll}px) rotate(${initscroll}deg) scale(${1+ initscroll/200})`
-        h1_2.style.opacity = 1 - initscroll/400;
-    }
+	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 200})`;
+	h1.style.opacity = 1 - scroll / 400;
+	let scroll_2 = 0;
 
+	//스크롤거리값이 2번째 섹션영역에 들어가면 scroll_2에는 scroll적용되는 값을 담고
+	//반대로 2번째 섹션영역을 벗어나면 scroll_2의 값을 0으로 초기화
+	scroll >= secs[1].offsetTop + baseLine ? (scroll_2 = scroll - secs[1].offsetTop - baseLine) : (scroll_2 = 0);
+
+	h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${1 + scroll_2 / 200})`;
+	h1_2.style.opacity = 1 - scroll_2 / 400;
 });
