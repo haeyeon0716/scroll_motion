3a9eb5dde5c20a7cf32993a0533bdb99dbc3f216
yhy7805 <yhy7805@gmail.com>
2023-09-27 10:12:39 +0900

    두번째 section 도달 했을 때 모션 시작

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ DOM.offset().top : 전체 페이지에서 특정 DOM의 세로 위치값 (정적
 */
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
+const h1_2 = secs[1].querySelector('h1');
 
 window.addEventListener('scroll', ()=>{
     const scroll = window.scrollY;
+    const scroll_2 = scroll - secs[1].offsetTop;
+
     h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1+ scroll/200})`
-    h1.style.opacity = 1 - (scroll/400)
+    h1.style.opacity = 1 - scroll/400;
+
+    if(scroll > secs[1].offsetTop){
+        h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${1+ scroll_2/200})`
+        h1_2.style.opacity = 1 - scroll_2/400;
+    }
 
 });
